b0490a6230fe97f18a0ef2bc0b230370a13867ad
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-17 22:42:13 +0800

    Initialized routing system with react-router, Root and Error page deployment

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./pages/App.tsx";
 import "./styles/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Root.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
