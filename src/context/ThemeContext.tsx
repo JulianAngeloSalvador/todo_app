14162c4262a4001f07e12d1c3f3d51a398551a6f
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-18 00:07:02 +0800

    Implemented Theme Context

---
+++ src/context/ThemeContext.tsx
@@ -0,0 +1,38 @@
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { ThemeInterface } from "../utils/interface";
+
+export const ThemeContext = createContext<ThemeInterface | null>(null);
+
+export default function ThemeContextProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error("useTheme must be used within the ThemeContextProvider");
+  }
+
+  return context;
+};
