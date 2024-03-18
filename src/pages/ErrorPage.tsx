ea04199d6b9c69ac2e6830559f0abfc043d24513
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-18 12:24:44 +0800

    Enabled default styling with 'styled' boolean prop

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -15,7 +15,7 @@ export default function ErrorPage() {
           <span className="absolute bg-secondary w-4/5 h-0.5 left-1/2 -translate-x-1/2 -bottom-2 rounded-full before:absolute before:bg-primary before:left-1/2 before:-translate-x-1/2 before:w-2 before:aspect-square before:outline before:outline-2 before:rotate-45 before:top-1/2 before:-translate-y-1/2 after:absolute after:left-1/2 after:-translate-x-1/2 after:top-1/2 after:-translate-y-1/2 after:bg-secondary after:w-1 after:aspect-square after:rotate-45" />
         </h1>
         <p className="text-subtitle mt-2">{error.statusText}</p>
-        <LinkButton to={"/"} text={"Back to homepage"} />
+        <LinkButton to={"/"} text={"Back to homepage"} styled={true} />
       </article>
     </section>
   );
