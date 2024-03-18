06486bd2bfbfd6ac9988f7b09ff03859a2554601
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-18 20:37:28 +0800

    Enabled className prop for conditional styling

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -2,12 +2,18 @@ import { Link } from "react-router-dom";
 import { LinkBtn } from "../utils/interface";
 
 export default function LinkButton(props: LinkBtn) {
-  const { to, text, styled } = props;
+  const { to, text, styled, className } = props;
 
   return (
     <Link
       to={to}
-      className={styled ? "cta-btn cta-base-pad rounded-lg text-sm" : ""}
+      className={
+        styled
+          ? "cta-btn cta-base-pad rounded-lg text-sm"
+          : className
+          ? className
+          : ""
+      }
     >
       {text}
     </Link>
