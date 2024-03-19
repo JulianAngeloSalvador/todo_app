88e2d2b24d953a35f71c9412321bc52a85094f8c
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-19 10:26:01 +0800

    Feat: deployed landing page within the App

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,5 @@
-import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
+import LandingPage from "./LandingPage";
 
 export default function App() {
-  return (
-    <>
-      <Header />
-      <Outlet />
-    </>
-  );
+  return <LandingPage />;
 }
