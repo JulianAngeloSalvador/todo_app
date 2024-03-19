59d2e56136ec89e12c2dbebb0be95979fb36782b
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-19 11:19:04 +0800

    Style: added cta color for brand name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,12 @@ export default function Header() {
     <header className="flex justify-center items-center p-4 px-8 fixed z-10 top-0 left-0 w-full min-h-20">
       <div className="flex items-center w-fluid-1200 justify-between">
         <h1 className="col-start-2">
-          <LinkButton to="/" text="Prime List" styled={false} />
+          <LinkButton
+            to="/"
+            text="Prime List"
+            styled={false}
+            className="font-extra-bold text-cta"
+          />
         </h1>
         <section
           className={`col-start-7 flex gap-x-4 items-center min-w-fit ${
