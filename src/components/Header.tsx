3bffb7de8beb0afa4ce9f752a765367b16d5a0ff
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-19 09:01:00 +0800

    Fix: header aspect & added padding for unstyled auth buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,40 +1,45 @@
 import { useLocation } from "react-router-dom";
 import LinkButton from "./LinkButton";
-import { useEffect } from "react";
 
 export default function Header() {
   const location = useLocation();
   let { pathname } = location;
 
   return (
-    <header className="flex justify-between items-center p-4 fixed z-10 top-0 left-0 w-full">
-      <h1>
-        <LinkButton to="/" text="Prime List" styled={false} />
-      </h1>
-      <section className="flex gap-x-4 items-center">
-        {pathname === "/" && (
-          <>
+    <header className="flex justify-center items-center p-4 px-8 fixed z-10 top-0 left-0 w-full min-h-20">
+      <div className="flex items-center w-fluid-1200 justify-between">
+        <h1 className="col-start-2">
+          <LinkButton to="/" text="Prime List" styled={false} />
+        </h1>
+        <section
+          className={`col-start-7 flex gap-x-4 items-center min-w-fit ${
+            pathname !== "/" && "justify-center"
+          }`}
+        >
+          {pathname === "/" && (
+            <>
+              <LinkButton
+                to="/login"
+                text="Sign In"
+                styled={false}
+                className="text-caption font-light cta-base-pad text-nowrap"
+              />
+              <LinkButton to="/register" text="Create Account" styled={true} />
+            </>
+          )}
+          {pathname === "/login" && (
+            <LinkButton to="/register" text="Create Account" styled={true} />
+          )}
+          {pathname === "/register" && (
             <LinkButton
               to="/login"
               text="Sign In"
               styled={false}
-              className="text-caption font-light"
+              className="text-caption font-light cta-base-pad"
             />
-            <LinkButton to="/register" text="Create Account" styled={true} />
-          </>
-        )}
-        {pathname === "/login" && (
-          <LinkButton to="/register" text="Create Account" styled={true} />
-        )}
-        {pathname === "/register" && (
-          <LinkButton
-            to="/login"
-            text="Sign In"
-            styled={false}
-            className="text-caption font-light"
-          />
-        )}
-      </section>
+          )}
+        </section>
+      </div>
     </header>
   );
 }
