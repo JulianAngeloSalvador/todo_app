282b747a91394cfc69206054987630cc08c11d66
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-21 19:13:29 +0800

    Feat: Implemented Dashboard index page with Outlet context from react-router

--- src/routes/Dashboard.tsx
+++ src/routes/Dashboard.tsx
@@ -1,7 +1,19 @@
+import { Outlet, useOutletContext } from "react-router-dom";
+import Sidenav from "../components/dashboard/Sidenav";
+import { useState } from "react";
+import { MenuState } from "../utils/interface";
+
 export default function Dashboard() {
+  const [collapsed, setCollapsed] = useState(false);
+
   return (
-    <section className="bg-primary min-h-dvh">
-      <h1>Dashboard</h1>
+    <section className="bg-primary min-h-dvh flex">
+      <Sidenav collapsed={collapsed} setCollapsed={setCollapsed} />
+      <Outlet context={{ collapsed, setCollapsed } satisfies MenuState} />
     </section>
   );
+}
+
+export function useMenuToggler() {
+  return useOutletContext<MenuState>();
 }
