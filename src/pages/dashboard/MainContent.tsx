27ea72a96d04983a792d5008c448c5789e253171
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-21 21:39:25 +0800

    Refactor: implemented conditional rendering of MenuToggler button comp depending on menu state

--- src/pages/dashboard/MainContent.tsx
+++ src/pages/dashboard/MainContent.tsx
@@ -4,13 +4,17 @@ import { MainProps } from "../../utils/interface";
 
 export default function MainContent(props: MainProps) {
   const { title, body } = props;
-  const { setCollapsed } = useMenuToggler();
+  const { collapsed, setCollapsed } = useMenuToggler();
 
   return (
     <section className="flex-grow flex-shrink-0">
-      <header>
-        <h2 className="border-b-2 text-title p-4">{title}</h2>
-        <MenuToggler setCollapsed={setCollapsed} />
+      <header
+        className={`border-b-2 p-4 flex items-center ${collapsed && "gap-x-2"}`}
+      >
+        <div className={!collapsed ? "scale-0 hidden" : "scale-100"}>
+          <MenuToggler setCollapsed={setCollapsed} />
+        </div>
+        <h2 className="text-title ">{title}</h2>
       </header>
       {body}
     </section>
