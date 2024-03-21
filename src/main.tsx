312d2039c6ad739fc59bae764ff74d8e01528d4b
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-21 19:15:54 +0800

    Feat: Added Dashboard routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Root from "./Root.tsx";
+import Root from "./routes/Root.tsx";
 import ErrorPage from "./pages/ErrorPage.tsx";
 import ThemeContextProvider from "./context/ThemeContext.tsx";
 import SignIn from "./components/SignIn.tsx";
 import SignUp from "./components/SignUp.tsx";
 import App from "./pages/App.tsx";
+import Dashboard from "./routes/Dashboard.tsx";
+import Todo from "./pages/dashboard/Todo.tsx";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +20,15 @@ const router = createBrowserRouter([
       { index: true, element: <App /> },
       { path: "/login", element: <SignIn /> },
       { path: "/register", element: <SignUp /> },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <Dashboard />,
+    children: [
+      { path: "/dashboard/todo", element: <Todo /> },
+      { path: "/dashboard/calendar" },
+      { path: "/dashboard/trash" },
     ],
   },
 ]);
