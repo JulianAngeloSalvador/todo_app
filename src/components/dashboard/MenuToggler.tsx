34367f03e112fbe7d76f72c78ec1b07d90491795
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-21 21:38:24 +0800

    Refactor: added styling and implemented conditional rendering of button icon depending on Icon prop

--- src/components/dashboard/MenuToggler.tsx
+++ src/components/dashboard/MenuToggler.tsx
@@ -1,15 +1,27 @@
 import { MenuState } from "../../utils/interface";
 
 export default function MenuToggler(props: MenuState) {
-  const { setCollapsed } = props;
+  const { setCollapsed, Icon } = props;
 
   const handleMenuState = () => {
     setCollapsed((prevState) => !prevState);
   };
+
+  const DefaultIcon = () => (
+    <>
+      <div className="w-full h-0.5" />
+      <div className="w-full h-0.5" />
+      <div className="w-full h-0.5" />
+    </>
+  );
   return (
     <button
       onClick={handleMenuState}
-      className="w-8 aspect-square bg-tertiary rounded-md"
-    />
+      className={`rounded-md w-6 aspect-square bg-tertiary items-center ${
+        !Icon && "justify-around children:bg-secondary flex flex-col p-1"
+      }`}
+    >
+      {Icon || DefaultIcon()}
+    </button>
   );
 }
