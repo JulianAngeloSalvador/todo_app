005fc1f832b3f3f4149060a980fb5a7e45786b72
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-23 22:09:43 +0800

    Feat: added reusable logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+export default function Logo() {
+  return (
+    <div className="w-[1em] h-[1em] aspect-auto bg-secondary flex items-center justify-around flex-col -rotate-45 rounded-sm p-0.5">
+      <div className="bg-primary w-[90%] h-0.5 rounded-md" />
+      <div className="bg-primary w-[90%] h-0.5 rounded-md" />
+    </div>
+  );
+}
