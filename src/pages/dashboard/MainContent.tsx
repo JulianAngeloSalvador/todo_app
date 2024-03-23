70712717326943b29920c007723913251c9fc4fb
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-23 16:17:00 +0800

    Refactor(MainContent): Removed subcomponent(MenuToggler) as functionality is no longer needed

--- src/pages/dashboard/MainContent.tsx
+++ src/pages/dashboard/MainContent.tsx
@@ -1,19 +1,20 @@
-import MenuToggler from "../../components/dashboard/MenuToggler";
-import { useMenuToggler } from "../../routes/Dashboard";
+// import MenuToggler from "../../components/dashboard/MenuToggler";
+// import { useMenuToggler } from "../../routes/Dashboard";
 import { MainProps } from "../../utils/interface";
 
 export default function MainContent(props: MainProps) {
   const { title, body } = props;
-  const { collapsed, setCollapsed } = useMenuToggler();
+  // const { collapsed, setCollapsed } = useMenuToggler();
 
   return (
     <section className="flex-grow flex-shrink-0">
       <header
-        className={`border-b-2 p-4 flex items-center ${collapsed && "gap-x-2"}`}
+        // className={`border-b-2 p-4 flex items-center ${collapsed && "gap-x-2"}`}
+        className={`border-b-2 p-4 flex items-center `}
       >
-        <div className={!collapsed ? "scale-0 hidden" : "scale-100"}>
+        {/* <div className={!collapsed ? "scale-0 hidden" : "scale-100"}>
           <MenuToggler setCollapsed={setCollapsed} />
-        </div>
+        </div> */}
         <h2 className="text-title ">{title}</h2>
       </header>
       {body}
