892cbd6209e38e0fb5a18cf0f66df957f57ae24d
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-23 16:10:24 +0800

    Feat: added Dashboard's index page

---
+++ src/pages/dashboard/DashboardIndex.tsx
@@ -0,0 +1,7 @@
+import MainContent from "./MainContent";
+
+export default function DashboardIndex() {
+  const Body = () => <p>Placeholder</p>;
+
+  return <MainContent title="Dashboard" body={Body()} />;
+}
