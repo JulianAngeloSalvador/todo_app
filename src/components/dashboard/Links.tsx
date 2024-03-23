e5263c363dc2a3fb58038b7af8e2c1d982ee0e6f
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-23 22:04:16 +0800

    Refactor: Links now render conditionally based on role prop & added links for footer links of sidenav

--- src/components/dashboard/Links.tsx
+++ src/components/dashboard/Links.tsx
@@ -1,14 +1,16 @@
 import { NavLink } from "react-router-dom";
 import { RxDashboard } from "react-icons/rx";
-import { LuListTodo, LuTrash2, LuCalendar } from "react-icons/lu";
+import { LuListTodo, LuTrash2, LuCalendar, LuSettings } from "react-icons/lu";
 import { capitalizeFirstChar } from "../../utils/functions";
+import { SidelinkProps } from "../../utils/interface";
+import Logo from "../Logo";
 
-const sidelinks = [
+const sidelinks: SidelinkProps[] = [
   {
     title: "dashboard",
     path: "/dashboard",
     icon: RxDashboard,
-    index: true,
+    index: "dashboardIndex",
   },
   {
     title: "todo",
@@ -27,34 +29,53 @@ const sidelinks = [
   },
 ];
 
+const footerlinks: SidelinkProps[] = [
+  {
+    title: "settings",
+    path: "/settings",
+    icon: LuSettings,
+  },
+  {
+    title: "Prime List",
+    path: "/",
+    icon: Logo,
+    index: "mainIndex",
+  },
+];
+
 interface LinkProp {
   collapsed?: boolean;
+  role: "main" | "footer";
 }
 
 export default function Links(props: LinkProp) {
-  const { collapsed } = props;
+  const { collapsed, role } = props;
+  const links = role === "main" ? sidelinks : footerlinks;
 
-  const Links = sidelinks.map((link) => {
+  const LinkItems = links.map((link) => {
     const { title, path, index } = link;
 
     return (
-      <li key={crypto.randomUUID()} className="">
+      <li
+        key={crypto.randomUUID()}
+        className={`hover:bg-secondary hover:text-primary rounded-md`}
+      >
         <NavLink
           to={!index ? `/dashboard${path}` : path}
-          className={`text-caption block relative p-4`}
+          className={`text-sm block relative py-2 px-3`}
         >
           <link.icon className="" />
           <span
-            className={`absolute top-1/2 -translate-y-1/2 left-10 transition-all duration-75 ease-in-out pt-1 ${
+            className={`absolute top-1/2 -translate-y-1/2 left-8 transition-all duration-75 ease-in-out pt-0.5 whitespace-nowrap ${
               collapsed ? "opacity-0" : "opacity-100"
             }`}
           >
-            {capitalizeFirstChar(title)}
+            {title && capitalizeFirstChar(title)}
           </span>
         </NavLink>
       </li>
     );
   });
 
-  return Links;
+  return LinkItems;
 }
