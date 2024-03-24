e66163d2434939b27c1c210cdcb3232c14c43898
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-24 13:53:57 +0800

    Refactor(Authenticator): increased form font size from text-sm to text-caption instead

--- src/components/Authenticator.tsx
+++ src/components/Authenticator.tsx
@@ -8,10 +8,10 @@ export default function Authenticator(props: AuthInterface) {
 
   return (
     <section className="min-h-dvh fixed inset-0 grid place-items-center px-6">
-      <div className="outline outline-1 outline-secondary/55 rounded-xl px-2 py-8 w-fluid-350 aspect-square flex flex-col justify-center items-center gap-4">
+      <div className="outline outline-1 outline-secondary/55 rounded-xl px-2 py-8 w-fluid-400 aspect-square flex flex-col justify-center items-center gap-4">
         <header className="text-center py-2">
-          <h3 className="text-subtitle">{title}</h3>
-          <p className="text-sm">{subtitle}</p>
+          <h3 className="text-title">{title}</h3>
+          <p className="text-caption">{subtitle}</p>
         </header>
         <main className="flex flex-col gap-4 w-4/5">
           <form action="" className="flex flex-col gap-4">
@@ -20,21 +20,21 @@ export default function Authenticator(props: AuthInterface) {
               name="email"
               id="email"
               placeholder="Username or Email"
-              className="text-sm cta-base-pad rounded-md leading-3 bg-transparent outline outline-1 -outline-offset-1 outline-secondary/55 focus-within:caret-secondary focus-within:outline-secondary hocus:outline-secondary"
+              className="cta-base-pad rounded-md leading-3 bg-transparent outline outline-1 -outline-offset-1 outline-secondary/55 focus-within:caret-secondary focus-within:outline-secondary hocus:outline-secondary"
             />
             <input
               type="password"
               placeholder="Password"
               name="password"
               id="password"
-              className="text-sm cta-base-pad rounded-md leading-3 bg-transparent outline outline-1 -outline-offset-1 outline-secondary/55 focus-within:caret-secondary focus-within:outline-secondary hocus:outline-secondary"
+              className=" cta-base-pad rounded-md leading-3 bg-transparent outline outline-1 -outline-offset-1 outline-secondary/55 focus-within:caret-secondary focus-within:outline-secondary hocus:outline-secondary"
             />
             {!newUser && (
               <small className="self-end px-2">Forgot Password?</small>
             )}
             <button
               onClick={(e) => e.preventDefault()}
-              className="cta-btn cta-base-pad rounded-md text-sm"
+              className="cta-btn cta-base-pad rounded-md"
             >
               {!newUser ? "Sign In" : "Sign Up"}
             </button>
@@ -43,14 +43,14 @@ export default function Authenticator(props: AuthInterface) {
             className="text-center relative
           after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-secondary after:w-full after:h-[1px] after:-z-10 my-4"
           >
-            <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 inline bg-primary px-2 text-xs">
+            <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 inline bg-primary px-2 ">
               OR
             </span>
           </small>
           <GoogleAuthenticator action={googleAction} />
         </main>
         <footer>
-          <p className="px-2 text-xs self-center font-light flex gap-x-2">
+          <p className="text-caption px-2 self-center font-light flex gap-x-2">
             {!newUser ? "Not a member yet?" : "Already have an account?"}
             <Link
               to={`${!newUser ? "/register" : "/login"}`}
