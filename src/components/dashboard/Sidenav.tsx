55ac4e0dc41cd78f2783e517d22d4b0baacb01be
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-24 14:12:51 +0800

    Refactor(Sidenav): increased uncollapsed basis to basis-56(224px) & adjusted MenuToggler's positioning relative to Sidenav

--- src/components/dashboard/Sidenav.tsx
+++ src/components/dashboard/Sidenav.tsx
@@ -10,7 +10,7 @@ export default function Sidenav(props: MenuState) {
   return (
     <aside
       className={`bg-tertiary top-0 left-0 h-dvh flex flex-col gap-4 p-2 flex-grow-0 flex-shrink-0 transition-all duration-100 ease-in-out relative group
-      ${collapsed ? "basis-10" : "basis-44"}`}
+      ${collapsed ? "basis-10" : "basis-56"}`}
     >
       {/* <header
         className={`flex items-center bg-primary rounded-md font-extra-bold text-sm relative p-1 ${
@@ -26,12 +26,12 @@ export default function Sidenav(props: MenuState) {
       <ul className="flex flex-col gap-2 mt-auto">
         <Links collapsed={collapsed} role="footer" />
       </ul>
-      <div className="bg-tertiary absolute -right-3 top-1/2 -translate-y-1/2 w-6 aspect-square rounded-full grid place-items-center outline-2 outline-secondary hover:outline cursor-pointer opacity-0 group-hover:opacity-100">
+      <div className="bg-tertiary absolute -right-4 top-1/2 -translate-y-1/2 w-8 aspect-square rounded-full grid place-items-center outline-3 outline-secondary hover:outline cursor-pointer opacity-0 group-hover:opacity-100">
         <MenuToggler
           setCollapsed={setCollapsed}
           Icon={
             <MdOutlineKeyboardDoubleArrowLeft
-              className={`w-3/4 h-3/4w-3/4 mx-auto ${
+              className={`w-3/4 h-3/4  mx-auto ${
                 collapsed ? "rotate-180" : "rotate-360"
               }`}
             />
