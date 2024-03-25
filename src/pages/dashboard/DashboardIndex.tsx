83cda2bd062ae6c3447e2818c0222baaf6932eec
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-25 14:38:39 +0800

    Refactor(Body): Renamed to DashboardContent for consistency

--- src/pages/dashboard/DashboardIndex.tsx
+++ src/pages/dashboard/DashboardIndex.tsx
@@ -1,7 +1,7 @@
 import MainContent from "./MainContent";
 
 export default function DashboardIndex() {
-  const Body = () => <p>Placeholder</p>;
+  const DashboardContent = () => <p>Placeholder</p>;
 
-  return <MainContent title="Dashboard" body={Body()} />;
+  return <MainContent title="Dashboard" body={DashboardContent()} />;
 }
