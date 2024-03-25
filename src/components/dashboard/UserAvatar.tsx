d788f15e9aa8ede59aa360bf9c725e68a9cb9044
tech.julian.salvador <tech.julian.salvador@gmail.com>
2024-03-25 21:18:10 +0800

    Feat: implemented a user avatar component for visualization

---
+++ src/components/dashboard/UserAvatar.tsx
@@ -0,0 +1,7 @@
+export default function UserAvatar() {
+  return (
+    <div className=" w-6 h-6 aspect-auto grid place-content-center outline outline-2 outline-secondary rounded-full ">
+      <div>A</div>
+    </div>
+  );
+}
